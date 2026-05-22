c020937a3a4ffa46b8ae07da3e8481d11aa7bf8a
MOUJITX <moujitx_chn@qq.com>
2026-05-22 05:25:01 +0000

    fix: added the missing import java.util.List; to InvoiceRepository.java

--- src/main/java/com/moujitx/homebox/server/repository/InvoiceRepository.java
+++ src/main/java/com/moujitx/homebox/server/repository/InvoiceRepository.java
@@ -6,6 +6,7 @@ import com.moujitx.homebox.server.enums.InvoiceStatus;
 import com.moujitx.homebox.server.enums.InvoiceType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
